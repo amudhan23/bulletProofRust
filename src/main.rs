b78b5f1fc94785243fc57532aabdb5e429c37278
amudhan23 <amudhanmuthaiah@yahoo.in>
2026-03-22 19:08:28 -0400

    Vector commitment

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,42 @@ use ark_bls12_381::{Fr, G1Projective};
 use ark_ec::Group;
 use ark_std::UniformRand;
 
-fn pederson_commitment(v:Fr, r:Fr, g:G1Projective, h:G1Projective) -> G1Projective{
-    (g*v) + (h*r)
+fn inner_product(a:&[Fr], b:&[Fr]) -> Fr{
+    a.iter()
+     .zip(b.iter())
+     .map(|(a_i, b_i)| *a_i * *b_i)
+     .sum()
+}
+
+fn pederson_commitment(a:&[Fr], b:&[Fr], g_vec: &[G1Projective], h_vec: &[G1Projective], u:G1Projective) -> G1Projective{
+
+    let c = inner_product(a,b);
+
+    let a_g : G1Projective = a.iter()
+                                .zip(g_vec.iter())
+                                .map(|(a_i, g_i)| *g_i * *a_i)
+                                .sum();
+    
+    let b_h : G1Projective = b.iter()
+                                .zip(h_vec.iter())
+                                .map(|(b_i, h_i)| *h_i * *b_i)
+                                .sum();
+
+    a_g + b_h + (u*c)                        
 }
 
 fn main(){
     let mut rng = ark_std::test_rng();
 
-    let g = G1Projective::generator();
+    let n=4;
 
-    let h = G1Projective::rand(&mut rng);
+    let a:Vec<Fr> = (0..n).map(|_| Fr::rand(&mut rng)).collect();
+    let b:Vec<Fr> = (0..n).map(|_| Fr::rand(&mut rng)).collect();
 
-    let v = Fr::from(5u32);
-    let r = Fr::rand(&mut rng);
+    let g_vec: Vec<G1Projective> = (0..n).map(|_| G1Projective::rand(&mut rng)).collect();
+    let h_vec: Vec<G1Projective> = (0..n).map(|_| G1Projective::rand(&mut rng)).collect();
 
-    // let commitment = pederson_commitment(v,r,g,h);
+    let u:G1Projective = G1Projective::rand(&mut rng);
 
-    println!("Successfully commited {}", pederson_commitment(v,r,g,h));
+    println!("Pedersen commitment: {}", pederson_commitment(&a, &b, &g_vec, &h_vec, u));
 }
