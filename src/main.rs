f775061ce62088b318f05eace44f1ab5f8b849f1
amudhan23 <amudhanmuthaiah@yahoo.in>
2026-03-22 19:45:21 -0400

    Generic curve

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,28 @@
 use ark_bls12_381::{Fr, G1Projective};
-use ark_ec::Group;
 use ark_std::UniformRand;
+use ark_ec::CurveGroup;
+use ark_ff::PrimeField;
 
-fn inner_product(a:&[Fr], b:&[Fr]) -> Fr{
+fn inner_product<F: PrimeField>(a:&[F], b:&[F]) -> F{
     a.iter()
      .zip(b.iter())
      .map(|(a_i, b_i)| *a_i * *b_i)
      .sum()
 }
 
-fn pederson_commitment(a:&[Fr], b:&[Fr], g_vec: &[G1Projective], h_vec: &[G1Projective], u:G1Projective) -> G1Projective{
+fn pederson_commitment<G: CurveGroup>(a:&[G::ScalarField], b:&[G::ScalarField], g_vec: &[G], h_vec: &[G], u:G) -> G{
 
     let c = inner_product(a,b);
 
-    let a_g : G1Projective = a.iter()
-                                .zip(g_vec.iter())
-                                .map(|(a_i, g_i)| *g_i * *a_i)
-                                .sum();
+    let a_g : G = a.iter()
+                        .zip(g_vec.iter())
+                        .map(|(a_i, g_i)| *g_i * *a_i)
+                        .sum();
     
-    let b_h : G1Projective = b.iter()
-                                .zip(h_vec.iter())
-                                .map(|(b_i, h_i)| *h_i * *b_i)
-                                .sum();
+    let b_h : G = b.iter()
+                        .zip(h_vec.iter())
+                        .map(|(b_i, h_i)| *h_i * *b_i)
+                        .sum();
 
     a_g + b_h + (u*c)                        
 }
