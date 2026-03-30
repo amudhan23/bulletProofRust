0fa36d476dee5ff1c9be6f97837ba3003e37e27b
amudhan23 <amudhanmuthaiah@yahoo.in>
2026-03-30 18:44:49 -0400

    merlin transcript

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,36 @@ use ark_bls12_381::{Fr, G1Projective};
 use ark_std::UniformRand;
 use ark_ec::CurveGroup;
 use ark_ff::PrimeField;
+use ark_serialize::CanonicalSerialize;
+use merlin::Transcript;
+
+
+pub trait TranscriptProtocol{
+    fn ipa_sep_domain(&mut self, n: u64);
+    fn append_point<G: CurveGroup>(&mut self, label: &'static [u8], point: &G);
+    fn get_challenge<F: PrimeField>(&mut self, label: &'static [u8]) -> F;
+}
+
+impl TranscriptProtocol for Transcript{
+
+    fn ipa_sep_domain(&mut self, n:u64){
+        self.append_message(b"dom-sep", b"NYU_Courant_IPA_v1");
+        self.append_u64(b"n", n);
+    }
+
+    fn append_point<G:CurveGroup>(&mut self, label: &'static[u8], point : &G){
+        let mut bytes = Vec::new();
+        point.serialize_compressed(&mut bytes).expect("Serialization failed");
+        self.append_message(label, &bytes);
+    }
+
+    fn get_challenge<F:PrimeField>(&mut self, label:&'static [u8]) -> F{
+        let mut buf = [0u8; 32];
+        self.challenge_bytes(label, &mut buf);
+        F::from_le_bytes_mod_order(&buf)
+    }
+
+}
 
 fn inner_product<F: PrimeField>(a:&[F], b:&[F]) -> F{
     a.iter()
@@ -40,5 +70,15 @@ fn main(){
 
     let u:G1Projective = G1Projective::rand(&mut rng);
 
-    println!("Pedersen commitment: {}", pederson_commitment(&a, &b, &g_vec, &h_vec, u));
+    let commitment = pederson_commitment(&a, &b, &g_vec, &h_vec, u);
+
+    let mut transcript = Transcript::new(b"Independent_Study_IPA");
+    transcript.ipa_sep_domain(n as u64);
+
+    transcript.append_point(b"C", &commitment);
+
+    let x:Fr = transcript.get_challenge(b"x");
+
+    println!("Successfully generated Challenge x: {}", x);
+
 }
